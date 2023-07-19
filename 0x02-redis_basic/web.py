0308c3cb70cabbb3d1584234b50379c5d4567005
emmakotey <emmakotey@gmail.com>
2023-07-19 19:12:04 +0000

    new

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,58 +1,22 @@
 #!/usr/bin/env python3
-
-"""
-This module provides a cache decorator that caches
-the result of a function for a certain amount of time
-and tracks how many times the function has been called.
-"""
-
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
+import redis
 import requests
-import time
-from functools import wraps
+r = redis.Redis()
+count = 0
 
 
-def cache(expiration_time: int) -> callable:
-    """
-    A decorator that caches the result of a function for
-    a certain amount of time
-    and tracks how many times the function has been called.
-
-    :param expiration_time: The expiration time of the cache in seconds.
-    :return: The decorated function.
-    """
-    def decorator(func: callable) -> callable:
-        cache_dict = {}
-        count_dict = {}
-
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            if url in cache_dict and time.time() -
-            cache_dict[url]["time"] < expiration_time:
-                print("Retrieving from cache")
-                cache_dict[url]["count"] += 1
-                count_dict[f"count:{url}"] += 1
-                return cache_dict[url]["content"]
-            else:
-                print("Retrieving from URL")
-                content = requests.get(url).content
-                cache_dict[url] = {"content": content, "time": time.time()}
-                count_dict[f"count:{url}"] = 1
-                return content
-
-        wrapper.cache_dict = cache_dict
-        wrapper.count_dict = count_dict
-        return wrapper
-
-    return decorator
+def get_page(url: str) -> str:
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
 
 
-@cache(10)
-def get_page(url: str) -> bytes:
-    """
-    Retrieves the content of a URL and caches it for a certain amount of time.
-
-    :param url: The URL to retrieve the content from.
-    :return: The content of the URL.
-    """
-    return requests.get(url).content
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')

0776911ff95709a7b62836c837a40e8fa206b9cc
emmakotey <emmakotey@gmail.com>
2023-07-19 19:10:08 +0000

    new

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 """
-This module provides a get_page function that retrieves the content of a URL and caches it for a certain amount of time.
+This module provides a cache decorator that caches
+the result of a function for a certain amount of time
+and tracks how many times the function has been called.
 """
 
 import requests
 import time
 from functools import wraps
 
+
 def cache(expiration_time: int) -> callable:
     """
-    A decorator that caches the result of a function for a certain amount of time and tracks how many times the function has been called.
+    A decorator that caches the result of a function for
+    a certain amount of time
+    and tracks how many times the function has been called.
 
     :param expiration_time: The expiration time of the cache in seconds.
     :return: The decorated function.
@@ -18,10 +23,12 @@ def cache(expiration_time: int) -> callable:
     def decorator(func: callable) -> callable:
         cache_dict = {}
         count_dict = {}
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             url = args[0]
-            if url in cache_dict and time.time() - cache_dict[url]["time"] < expiration_time:
+            if url in cache_dict and time.time() -
+            cache_dict[url]["time"] < expiration_time:
                 print("Retrieving from cache")
                 cache_dict[url]["count"] += 1
                 count_dict[f"count:{url}"] += 1
@@ -32,10 +39,13 @@ def cache(expiration_time: int) -> callable:
                 cache_dict[url] = {"content": content, "time": time.time()}
                 count_dict[f"count:{url}"] = 1
                 return content
+
         wrapper.cache_dict = cache_dict
         wrapper.count_dict = count_dict
         return wrapper
+
     return decorator
+
 
 @cache(10)
 def get_page(url: str) -> bytes:

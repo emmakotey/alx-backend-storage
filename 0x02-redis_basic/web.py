a5a8dbf5894cebc5b924de36685e9ea5dbd8415e
emmakotey <emmakotey@gmail.com>
2023-07-20 07:19:38 +0000

    note

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,20 +3,31 @@
     obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
+
 r = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
+    """ Track how many times a particular URL was accessed in the key
+        "count:{url}" and cache thei expiration time of 10 seconds """
+    count_key = f"count:{url}"
+    cached_key = f"cached:{url}"
+
+    # Increment the count
+    r.incr(count_key)
+    count = r.get(count_key)
+
+    # Cache the result with an expiration time of 10 seconds
+    if r.exists(cached_key):
+        return r.get(cached_key).decode("utf-8")
+
     resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    r.setex(cached_key, 10, resp.text)
+
     return resp.text
 
 
 if __name__ == "__main__":
+    # Call get_page with the URLs to cache them
+    get_page('http://google.com')
     get_page('http://slowwly.robertomurray.co.uk')
